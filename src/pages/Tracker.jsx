5e45ebc650778be7239c3925843aa4742eb22c8c
anirbansanu <anirbanmukherjee602@gmail.com~>
2022-12-21 20:21:59 +0530

    call api ready

--- src/pages/Tracker.jsx
+++ src/pages/Tracker.jsx
@@ -14,7 +14,9 @@ export default class Tracker extends Component {
             stateData: [],
             stateMeta: []
         }
-        this.getCovidData = this.getCovidData.bind(this);
+        this.getData = this.getData.bind(this);
+        this.updateData = this.updateData.bind(this);
+        // this.program = this.program.bind(this);
         this.handleShowModal = this.handleShowModal.bind(this);
         this.handleCovidDataByState = this.handleCovidDataByState.bind(this);
     }
@@ -25,16 +27,17 @@ export default class Tracker extends Component {
             stateMeta: passStateData.meta
         });
     }
-    async getCovidData(){
+    async getData(key){
         try{
-            const res = await fetch("https://data.covid19india.org/v4/min/data.min.json");
+            const res = await fetch("http://pytalk.c1.biz/api/js/read_all.php?key="+key);
             const actualData = await res.json();
-            //console.log(actualData);
-            this.setState({
-                data:actualData,
-                stateData:actualData.WB.total,
-                stateMeta: actualData.WB.meta
-            });
+            console.log(actualData);
+            return actualData;
+            // this.setState({
+            //     data:actualData,
+            //     stateData:actualData.WB.total,
+            //     stateMeta: actualData.WB.meta
+            // });
             // console.log(actualData);
             // console.log(this.state.stateData);
             // console.log(this.state.stateMeta);
@@ -42,13 +45,50 @@ export default class Tracker extends Component {
         catch(err){
             console.log(err);
         }
-        
+        return null;
     }
-    componentDidMount(){
-        this.getCovidData();
+    async updateData(key,value){
+        try{
+            const res = await fetch("http://pytalk.c1.biz/api/js/update.php?key="+key+"&text="+value);
+            console.log(res);
+            const actualData = await res.json();
+            console.log(actualData);
+            return actualData;
+        }
+        catch(err){
+            console.log(err);
+        }
+        return null;
     }
+    // componentDidMount(){
+    //     this.getCovidData();
+    // }
     handleShowModal (){ 
         this.setState({showModal:true});
+    }
+    program(status){
+        status = status.toLowerCase();
+        let st=0;
+        console.log("program() running...");
+        switch(status) {
+            case "stop":    
+                st=0;
+                break;
+            case "start":   
+                st=1;
+                break;
+            case "kill":    
+                st=2;
+                break;
+            default:
+                st=1;
+                break;
+        }
+        console.log('Status : ');
+        console.log(status);
+        let data = this.updateData("redmi123",st);
+        console.log("data : "+data);
+        console.log("st : "+st);
     }
     render() {
         return (
@@ -66,14 +106,14 @@ export default class Tracker extends Component {
                     </Col>
                 </Row>
                 <Row>
-                    <Col className="mt-3" sm={6} md={6} lg={4} onClick={() => this.setState({showModal:true})}>
+                    <Col className="mt-3" sm={6} md={6} lg={4} >
                         <TrackerCard bg="#fa8231" smTitle="Power" lgTitle='On/Off' mainFontSize='2.5rem' tooltip={this.state.stateName} main={this.state.stateName.substr(0, 11)}/>
                     </Col>
-                    <Col className="mt-3" sm={6} md={6} lg={4}>
-                        <TrackerCard bg="#20bf6b" smTitle="Controller" lgTitle='Start' mainFontSize='3rem' main={this.state.stateData.tested}/>
+                    <Col className="mt-3" sm={6} md={6} lg={4} onClick={() => this.program("start")} >
+                        <TrackerCard bg="#20bf6b" smTitle="Controller" lgTitle='Start' mainFontSize='3rem' />
                     </Col>
-                    <Col className="mt-3" sm={6} md={6} lg={4}>
-                        <TrackerCard bg="#f7b731" smTitle="Controller" lgTitle='Stop' mainFontSize='3rem' main={this.state.stateData.confirmed}/>
+                    <Col className="mt-3" sm={6} md={6} lg={4} onClick={() => this.program("stop")} >
+                        <TrackerCard bg="#f7b731" smTitle="Controller" lgTitle='Stop' mainFontSize='3rem'/>
                     </Col>
                 
                     <Col className="mt-3" sm={6} md={6} lg={4}>
